bc9a3a84b80807b429f58ed0b0d778d942bd3b82
huangxusijia <1321799482@qq.com>
2016-01-26 22:05:59 +0800

    ###

---
+++ 1001A+B.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int c=a+b;
+	if(c<0)
+	{
+	printf("-");
+	c=-c;
+	} 
+	if(c<1000)
+	printf("%d\n",c);
+	else if(c<1000000)
+	printf("%d,%03d\n",c/1000,c%1000);
+	else 
+	printf("%d,%03d,%03d\n",c/1000000,c%1000000/1000,c%1000);
+	return 0;
+	 } 
