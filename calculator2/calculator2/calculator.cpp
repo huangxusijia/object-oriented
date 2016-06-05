8f41124df8b99ea25da3aee11b1bf62e913882fe
huangxusijia <1321799482@qq.com>
2016-06-05 22:04:14 +0800

    t

--- calculator2/calculator2/calculator.cpp
+++ calculator2/calculator2/calculator.cpp
@@ -3,16 +3,29 @@
 #include<string>
 #include"Scan.h"
 #include"Print.h"
+#include"cal.h"
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	string input;
+	string input,input1;
 	cin >> input;
 	Scan get;
-	queue<string> f = get.inputqueue(input);
-	Print out;
-	out.printqueue(f);
+	cal calc;
+	if (input == "-a")
+	{
+		cin >> input1;
+		cout << input1 << "= ";
+		queue<string> f = get.inputqueue(input1);
+		cout << calc.caluat(f) << endl;
+	}
+	else
+	{
+    queue<string> f = get.inputqueue(input);
+	cout << calc.caluat(f) << endl;
+	}
+	//Print out;
+	//out.printqueue(f);
 	system("pause");
 	return 0;
 }
